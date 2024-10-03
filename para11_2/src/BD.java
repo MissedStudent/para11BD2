47d3b892d4f42dca85a279bb047e44762cb49807
MissedStudent <evgenii.kuzovlev2005@gmail.com>
2024-10-03 11:20:08 +0300

    Add files via upload

---
+++ para11_2/src/BD.java
@@ -0,0 +1,240 @@
+import java.sql.*;
+import java.util.Random;
+
+public class BD {
+    public static String url ="jdbc:mysql://localhost/groups";
+    public static String user = "root";
+    public static String password ="";
+    // открыть соединение
+    public Connection openConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+    //закрыть соединение
+    public void closeConnection(Connection connection){
+        if (connection!=null)
+        {
+            try
+            {
+                connection.close();
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void addStudent(String student_surname, String student_name, String student_lastname, String student_group, int student_variant){
+        Connection connection = null;
+        try {
+            connection = openConnection();//INSERT INTO `students` (`id_student`, `student_surname`, `student_name`, `student_lastname`, `student_group`, `student_variant`) VALUES (NULL, 'Власов', 'Радий', 'Андреевич', 'ИС-22', '1');
+            String query = "INSERT INTO `students` (`student_surname`, `student_name`, `student_lastname`, `student_group`, `student_variant`) VALUES (?, ?, ?, ?, ?);";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1,student_surname);
+            statement.setString(2,student_name);
+            statement.setString(3,student_lastname);
+            statement.setString(4,student_group);
+            statement.setInt(5,student_variant);
+            int rowsTrue=statement.executeUpdate();
+            if(rowsTrue>0) {System.out.println("Добавлен");}
+            else {System.out.println("Не добавлен");}
+            closeConnection(connection);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            closeConnection(connection);
+        }
+    }
+    public void variantStudents(){
+        Connection connection = null;
+        Random random = new Random();
+        try {
+            connection = openConnection();
+            int[] arr=new int[35];
+            int ar=1;
+            int one = 0,two=0,three=0,four=0,five=0,six=0,seven=0,eight=0,nine=0;
+            for (int i=0;i<arr.length;i++) {
+                arr[i] = random.nextInt(10);
+                if (arr[i]==0){arr[i]=10;}
+                if (arr[i]==1){
+                    one++;
+                    if (one>4){arr[i]=2;}
+                }
+                if (arr[i]==2){
+                    two++;
+                    if (two>4){arr[i]=3;}
+                }
+                if (arr[i]==3){
+                    three++;
+                    if (three>4){arr[i]=4;}
+                }
+                if (arr[i]==4){
+                    four++;
+                    if (four>4){arr[i]=5;}
+                }
+                if (arr[i]==5){
+                    five++;
+                    if (five>4){arr[i]=6;}
+                }
+                if (arr[i]==6){
+                    six++;
+                    if (six>4){arr[i]=7;}
+                }
+                if (arr[i]==7){
+                    seven++;
+                    if (seven>4){arr[i]=8;}
+                }
+                if (arr[i]==8){
+                    eight++;
+                    if (eight>4){arr[i]=9;}
+                }
+                if (arr[i]==9){
+                    nine++;
+                    if (nine>4){arr[i]=10;}
+                }
+                String query = "UPDATE `students` SET `student_variant` = ? WHERE `students`.`id_student` = ?";
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setInt(1,arr[i]);
+                statement.setInt(2,ar);
+                int rowsTrue = statement.executeUpdate();
+                if (rowsTrue > 0) {
+                    System.out.println("Добавлен");
+                } else {
+                    System.out.println("Не добавлен");
+                }
+                ar++;
+            }
+            closeConnection(connection);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            closeConnection(connection);
+        }
+    }
+    public void sortStudents(){
+        Connection connection = null;
+        try {
+            connection = openConnection();
+            String query = "SELECT student_surname,student_name,student_lastname FROM `students` ORDER BY `students`.`student_surname` ASC";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next())
+            {
+                String surnam = resultSet.getString("student_surname");
+                String nam = resultSet.getString("student_name");
+                String group_name = resultSet.getString("student_lastname");
+                System.out.println(surnam + " "+nam+" "+group_name);
+            }
+            closeConnection(connection);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            closeConnection(connection);
+        }
+    }
+    public void studentsByVariants(){
+        Connection connection = null;
+        try {
+            connection = openConnection();//SELECT * FROM `students` ORDER BY `students`.`student_variant` ASC
+            String query = "SELECT student_surname,student_name,student_lastname,student_variant FROM `students` ORDER BY `students`.`student_variant` ASC";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next())
+            {
+                String surnam = resultSet.getString("student_surname");
+                String nam = resultSet.getString("student_name");
+                String group_name = resultSet.getString("student_lastname");
+                System.out.println(surnam + " "+nam+" "+group_name);
+            }
+            closeConnection(connection);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            closeConnection(connection);
+        }
+    }
+    public void variantByStudent(String student_surname){
+        Connection connection = null;
+        try {
+            connection = openConnection();//SELECT * FROM `students` ORDER BY `students`.`student_variant` ASC
+            String query = "SELECT student_surname,student_variant FROM `students` WHERE student_surname=?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1,student_surname);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next())
+            {
+                String surnam = resultSet.getString("student_surname");
+                int var = resultSet.getInt("student_variant");
+                System.out.println(surnam + " "+var);
+            }
+            closeConnection(connection);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            closeConnection(connection);
+        }
+    }
+    public void listOfStudentsByVariant(int variant){
+        Connection connection = null;
+        try {
+            connection = openConnection();//SELECT * FROM `students` ORDER BY `students`.`student_variant` ASC
+            String query = "SELECT student_surname,student_name,student_lastname,student_variant FROM `students` WHERE student_variant=?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1,variant);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next())
+            {
+                String surnam = resultSet.getString("student_surname");
+                String nam = resultSet.getString("student_name");
+                String group_name = resultSet.getString("student_lastname");
+                int var = resultSet.getInt("student_variant");
+                System.out.println(surnam +  " "+nam+" "+group_name+" "+var);
+            }
+            closeConnection(connection);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            closeConnection(connection);
+        }
+    }
+    public void numberOfStudentsByVariants(){
+        Connection connection = null;
+        try {
+            connection = openConnection();//SELECT * FROM `students` ORDER BY `students`.`student_variant` ASC
+            String query = "SELECT student_variant,COUNT(student_variant) AS student_count FROM `students` GROUP BY student_variant ORDER BY `students`.`student_variant` ASC";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next())
+            {
+                String surnam = resultSet.getString("student_variant");
+                String nam = resultSet.getString("student_count");
+                System.out.println(surnam +  " "+nam);
+            }
+            closeConnection(connection);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            closeConnection(connection);
+        }
+    }
+}
---
+++ para11_2/src/Main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Main {
+    public static BD bd=new BD();
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        boolean bool=true;
+        while (bool) {
+            System.out.println();
+            System.out.println("""
+                    1.Равномерное распределение вариантов среди студентов от 1 до 10
+                    2.Вывод отсортированного списка студентов с вариантами
+                    3.Вывод списка студентов сгруппированных по вариантам
+                    4.Вывод варианта студента по фамилии
+                    5.Вывод списка студентов с определенным вариантом
+                    6.Вывод количества студентов с каждым вариантом
+                    7.Завершение работы программы
+                    """);
+            int swtch = scanner.nextInt();
+            switch (swtch) {
+                case 10: {
+                    boolean boolik = true;
+                    while (boolik) {
+                        System.out.println("1 или 2");
+                        int boolswtch = scanner.nextInt();
+                        switch (boolswtch) {
+                            case 1: {
+                                System.out.println("Фамилия");
+                                String surname = scanner.next();
+                                System.out.println("Имя");
+                                String name = scanner.next();
+                                System.out.println("Отчество");
+                                String lastname = scanner.next();
+                                System.out.println("Группа");
+                                String group = scanner.next();
+                                System.out.println("Вариант");
+                                int var = scanner.nextInt();
+                                bd.addStudent(surname, name, lastname, group, var);
+                                System.out.println();
+                            }
+                            break;
+                            case 2: {
+                                bool = false;
+                            }
+                            break;
+                        }
+                    }
+                }
+                break;
+                case 1: {//1.Равномерное распределение вариантов среди студентов от 1 до 10
+                    bd.variantStudents();
+                    System.out.println("Сделано");
+                }
+                break;
+                case 2: {//2.Вывод отсортированного списка студентов с вариантами
+                    bd.sortStudents();
+                }
+                break;
+                case 3: {//3.Вывод списка студентов сгруппированных по вариантам
+                    bd.studentsByVariants();
+                }
+                break;
+                case 4: {//4.Вывод варианта студента по фамилии
+                    System.out.println("Введите фамилию студента");
+                    String fam = scanner.next();
+                    bd.variantByStudent(fam);
+                }
+                break;
+                case 5: {//5.Вывод списка студентов с определенным вариантом
+                    System.out.println("Введите вариант");
+                    int varik=scanner.nextInt();
+                    bd.listOfStudentsByVariant(varik);
+                }
+                break;
+                case 6: {//6.Вывод количества студентов с каждым вариантом
+                    bd.numberOfStudentsByVariants();
+                }
+                break;
+                case 7:{
+                    bool=false;
+                }break;
+            }
+        }
+    }
+}
